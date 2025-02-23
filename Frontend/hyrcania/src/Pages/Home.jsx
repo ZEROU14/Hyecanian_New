cf265e36a748389e981f204cb1ebe53dea66d625
hemant-thawani <hemantthawani57@gmail.com>
2025-02-23 19:43:52 +0530

    irony

--- Frontend/hyrcania/src/Pages/Home.jsx
+++ Frontend/hyrcania/src/Pages/Home.jsx
@@ -1,30 +1,28 @@
 import EventCard from "@/components/EventCard/EventCard";
 import DiscoveryIcon from "@/assets/DiscoveryIcon";
 import Runner from "@/components/images/headerBg.jpg";
+import MarathonDetail from "./MarathonDetail";
+
 const Home = () => {
   return (
     <div
-      style={{
-        background: "#F6F5F2",
-      }}
+      style={{ background: "#F6F5F2" }}
       data-oid="_444160"
-      className = "relative overflow-auto min-h-screen"
+      className="relative overflow-auto min-h-screen"
     >
-     
-     <div>
-     <img src={Runner} alt="a man running" className="w-full h-full object-cover" />
-     <div className="absolute inset-0 bg-gradient-to-l from-[#E6E2C3] via-[#88A47C] via-50% to-[#1C315E] opacity-50"></div>
-     <div className="absolute top-96 left-1/3 text-white text-6xl font-bold z-10">Run Fast Run Free</div>
-     </div>
+      <div>
+        <img src={Runner} alt="a man running" className="w-full h-full object-cover" />
+        <div className="absolute inset-0 bg-gradient-to-l from-[#E6E2C3] via-[#88A47C] via-50% to-[#1C315E] opacity-50"></div>
+        <div className="absolute top-96 left-1/3 text-white text-6xl font-bold z-10">
+          Run Fast Run Free
+        </div>
+      </div>
 
-      <div className="flex flex-col items-center   " data-oid="-hf6mfu">
-        <div
-          className="flex flex-row self-start space-x-4 ml-20 pl-6 mt-10 "
-          data-oid="64oggzd"
-        >
+      <div className="flex flex-col items-center" data-oid="-hf6mfu">
+        <div className="flex flex-row self-start space-x-4 ml-20 pl-6 mt-10" data-oid="64oggzd">
           <DiscoveryIcon data-oid="0ordo.3" />
           <h1
-            className=" text-left text-3xl text-black-800 "
+            className="text-left text-3xl text-black-800"
             style={{
               color: "#212121",
               fontStyle: "normal",
@@ -37,19 +35,29 @@ const Home = () => {
           </h1>
         </div>
 
-        <div
-          className="grid grid-cols-1   sm:grid-cols-2 lg:grid-cols-3 "
-          data-oid="-_k11_."
-        >
-          <EventCard data-oid=".:_8mc:" />
-          <EventCard data-oid="u.y2m0:" />
-          <EventCard data-oid="hwl9:0b" />
-          <EventCard data-oid="y.vdz87" />
-          <EventCard data-oid="o4exsff" />
-          <EventCard data-oid=":s4:v0u" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3" data-oid="-_k11_.">
+          <a href="/blog">
+            <EventCard data-oid=".:_8mc:" />
+          </a>
+          <a href="/blog">
+            <EventCard data-oid="u.y2m0:" />
+          </a>
+          <a href="/blog">
+            <EventCard data-oid="hwl9:0b" />
+          </a>
+          <a href="/blog">
+            <EventCard data-oid="y.vdz87" />
+          </a>
+          <a href="/blog">
+            <EventCard data-oid="o4exsff" />
+          </a>
+          <a href="/blog">
+            <EventCard data-oid=":s4:v0u" />
+          </a>
         </div>
       </div>
     </div>
   );
 };
+
 export default Home;
